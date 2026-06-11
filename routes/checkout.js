4a4e1bee0ccb092dbdb1fb0d6cacf0d5fa9e89e3
anandjbangad <anandjbangad@gmail.com>
2026-06-11 15:10:41 -0500

    Use environment variables for transaction details

--- routes/checkout.js
+++ routes/checkout.js
@@ -57,9 +57,9 @@ router.post('/', (req, res, next) => {
   const nonceFromTheClient = req.body.paymentMethodNonce;
   // Create a new transaction for $10
   const newTransaction = gateway.transaction.sale({
-    amount: '1.00',
+    amount: process.env.AMOUNT,
     paymentMethodNonce: nonceFromTheClient,
-    merchantAccountId: "BT-FZ-BNZ",
+    merchantAccountId: process.env.MERCHANT_ACCOUNT_ID,
     options: {
       // This option requests the funds from the transaction
       // once it has been authorized successfully
